3bfb3704e0c1ff01602fca08ee39048c0427284c
liuYukun260 <2528075388@qq.com>
2025-12-16 11:27:13 +0800

    算法code练习

---
+++ main/206/反转链表/Solution.java
@@ -0,0 +1,23 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre=null;
+        ListNode cur=head;
+        while (cur!=null){
+            ListNode next=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=next;
+        }
+        return pre;
+    }
+}
